c26980db16921367b37ecd3d5a4498cc0f180fd7
baijiezhen <baijz@winning.com.cn>
2020-05-09 14:04:20 +0800

    初步搭建home页面，安装路由

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+// import AuthRoute from "./components/AuthRoute";
+// import Home from "./pages/Home/index";
+import Home from "./pages/Home/index";
+// import CityList from "./pages/CityList/index";
+// import Map from "./pages/Map/index";
+// import HouseDetail from "./pages/HouseDetail/index";
+// import Login from "./pages/Login/index";
+// import Profile from "./pages/Profile/index";
+// import Test from "./pages/Test/index";
+// import Todo from "./pages/Todo/index";
+// import Hooks from "./pages/Hooks/index";
+// import "react-virtualized/styles.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/" exact render={() => <Redirect to="/home" />}></Route>
+        {/* <Link to="/citylist">城市列表</Link> */}
+        <Route path="/home" component={Home}></Route>
+        {/* <Route path="/citylist" component={CityList}></Route>
+        <AuthRoute path="/map" component={Map}></AuthRoute>
+        <Route path="/detail/:id" component={HouseDetail}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route path="/Profile" component={Profile}></Route>
+        <Route path="/test" component={Test}></Route>
+        <Route path="/Todo" component={Todo}></Route>
+        <Route path="/Hooks" component={Hooks}></Route> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return <div>11111home</div>;
+  }
+}
+export default Home;
